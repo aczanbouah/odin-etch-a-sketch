c50131abe98733eab5de1494cbb9a1122811188e
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-18 18:13:49 +0200

    Add functionality to dynamically create grid based on slider value

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-// box size = container width / number of boxes
-
-const gridContainer = document.querySelector(".container");
+const gridContainer = document.querySelector(".grid-container");
+const GRID_CONTAINER_WIDTH = 480;
+const gridSizeSlider = document.querySelector("#slider");
 
 function createGridItem() {
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
-  gridItem.style.width = 120 + "px";
-  gridItem.style.height = 120 + "px";
+  gridItem.style.width = `${GRID_CONTAINER_WIDTH / gridSizeSlider.value}px`;
+  gridItem.style.height = `${GRID_CONTAINER_WIDTH / gridSizeSlider.value}px`;
   gridContainer.appendChild(gridItem);
 }
 
-for (let i = 0; i < 16; i++) {
-  createGridItem();
+function createGrid() {
+  for (let i = 0; i < gridSizeSlider.value ** 2; i++) {
+    createGridItem();
+  }
 }
+
+function resetGrid() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+  createGrid();
+}
+
+gridSizeSlider.addEventListener("mouseup", resetGrid);
+
+createGrid();

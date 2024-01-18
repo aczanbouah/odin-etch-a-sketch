eb9424f1850589fc43fe97c73efe2d827beaea28
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2024-01-18 19:10:26 +0200

    Add functionality to change background color of pixel when hovered over

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
 const GRID_CONTAINER_WIDTH = 480;
 const gridSizeSlider = document.querySelector("#slider");
+const colorPicker = document.querySelector("#color-picker");
+const gridSizeLabel = document.querySelector("#grid-size-label");
 
 function createGridItem() {
   const gridItem = document.createElement("div");
@@ -8,12 +10,16 @@ function createGridItem() {
   gridItem.style.width = `${GRID_CONTAINER_WIDTH / gridSizeSlider.value}px`;
   gridItem.style.height = `${GRID_CONTAINER_WIDTH / gridSizeSlider.value}px`;
   gridContainer.appendChild(gridItem);
+  gridItem.addEventListener("mouseover", function () {
+    gridItem.style.backgroundColor = `${colorPicker.value}`;
+  });
 }
 
 function createGrid() {
   for (let i = 0; i < gridSizeSlider.value ** 2; i++) {
     createGridItem();
   }
+  gridSizeLabel.innerText = `Grid size: ${gridSizeSlider.value} x ${gridSizeSlider.value}`;
 }
 
 function resetGrid() {
@@ -23,6 +29,6 @@ function resetGrid() {
   createGrid();
 }
 
-gridSizeSlider.addEventListener("mouseup", resetGrid);
-
 createGrid();
+
+gridSizeSlider.addEventListener("mouseup", resetGrid);
